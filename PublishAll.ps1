141f2b999ed5b5fa6168fdb525c6afef7a2afd39
Sewer 56 <seweratwork@gmail.com>
2022-10-10 02:01:46 +0100

    Added: Changelog to CI/CD builds.

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -10,7 +10,8 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
               -PublishOutputDir "Publish/ToUpload/Redirector" `
               -MakeDelta true -UseGitHubDelta true `
               -MetadataFileName "Reloaded.Universal.Redirector.ReleaseMetadata.json" `
-              -GitHubUserName Reloaded-Project -GitHubRepoName reloaded.universal.redirector -GitHubFallbackPattern reloaded.universal.redirector.zip -GitHubInheritVersionFromTag false
+              -GitHubUserName Reloaded-Project -GitHubRepoName reloaded.universal.redirector -GitHubFallbackPattern reloaded.universal.redirector.zip -GitHubInheritVersionFromTag false `
+			  @args
 
 ./Publish.ps1 -ProjectPath "Reloaded.Universal.Monitor/Reloaded.Universal.Monitor.csproj" `
               -PackageName "Reloaded.Universal.Monitor" `
@@ -18,7 +19,8 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
               -PublishOutputDir "Publish/ToUpload/Monitor" `
               -MakeDelta true -UseGitHubDelta true `
               -MetadataFileName "Reloaded.Universal.Monitor.ReleaseMetadata.json" `
-              -GitHubUserName Reloaded-Project -GitHubRepoName reloaded.universal.redirector -GitHubFallbackPattern reloaded.universal.monitor.zip -GitHubInheritVersionFromTag false
+              -GitHubUserName Reloaded-Project -GitHubRepoName reloaded.universal.redirector -GitHubFallbackPattern reloaded.universal.monitor.zip -GitHubInheritVersionFromTag false `
+			  @args
 
 ./Publish.ps1 -ProjectPath "Reloaded.Universal.RedirectorMonitor/Reloaded.Universal.RedirectorMonitor.csproj" `
               -PackageName "Reloaded.Universal.RedirectorMonitor" `
@@ -26,6 +28,7 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
               -PublishOutputDir "Publish/ToUpload/RedirectorMonitor" `
               -MakeDelta true -UseGitHubDelta true `
               -MetadataFileName "Reloaded.Universal.RedirectorMonitor.ReleaseMetadata.json" `
-              -GitHubUserName Reloaded-Project -GitHubRepoName reloaded.universal.redirector -GitHubFallbackPattern reloaded.universal.redirectormonitor.zip -GitHubInheritVersionFromTag false
+              -GitHubUserName Reloaded-Project -GitHubRepoName reloaded.universal.redirector -GitHubFallbackPattern reloaded.universal.redirectormonitor.zip -GitHubInheritVersionFromTag false `
+			  @args
 
 Pop-Location
