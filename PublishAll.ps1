27785f6ce6c4c72870bffd5b0fb49f6b1e651bea
Sewer 56 <seweratwork@gmail.com>
2022-10-10 02:12:28 +0100

    Fixed: Relative Readme Paths for Projects

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -6,7 +6,7 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 
 ./Publish.ps1 -ProjectPath "Reloaded.Universal.Redirector/Reloaded.Universal.Redirector.csproj" `
               -PackageName "Reloaded.Universal.Redirector" `
-			  -ReadmePath README-REDIRECTOR.md `
+			  -ReadmePath ./README-REDIRECTOR.md `
               -PublishOutputDir "Publish/ToUpload/Redirector" `
               -MakeDelta true -UseGitHubDelta true `
               -MetadataFileName "Reloaded.Universal.Redirector.ReleaseMetadata.json" `
@@ -15,7 +15,7 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 
 ./Publish.ps1 -ProjectPath "Reloaded.Universal.Monitor/Reloaded.Universal.Monitor.csproj" `
               -PackageName "Reloaded.Universal.Monitor" `
-			  -ReadmePath README-MONITOR.md `
+			  -ReadmePath ./README-MONITOR.md `
               -PublishOutputDir "Publish/ToUpload/Monitor" `
               -MakeDelta true -UseGitHubDelta true `
               -MetadataFileName "Reloaded.Universal.Monitor.ReleaseMetadata.json" `
@@ -24,7 +24,7 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 
 ./Publish.ps1 -ProjectPath "Reloaded.Universal.RedirectorMonitor/Reloaded.Universal.RedirectorMonitor.csproj" `
               -PackageName "Reloaded.Universal.RedirectorMonitor" `
-			  -ReadmePath README-REDIRECTORMONITOR.md `
+			  -ReadmePath ./README-REDIRECTORMONITOR.md `
               -PublishOutputDir "Publish/ToUpload/RedirectorMonitor" `
               -MakeDelta true -UseGitHubDelta true `
               -MetadataFileName "Reloaded.Universal.RedirectorMonitor.ReleaseMetadata.json" `
