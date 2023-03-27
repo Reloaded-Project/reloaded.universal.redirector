0a662f0527cc5156f819d0191d90ace5dc18af93
Sewer 56 <seweratwork@gmail.com>
2023-03-27 09:42:02 +0100

    Added: Benchmark for getting current thread ID natively

--- Reloaded.Universal.Redirector.Benchmarks/Benchmarks/RecursionLockCandidatesBenchmark.cs
+++ Reloaded.Universal.Redirector.Benchmarks/Benchmarks/RecursionLockCandidatesBenchmark.cs
@@ -1,11 +1,12 @@
 using BenchmarkDotNet.Attributes;
 using Reloaded.Universal.Redirector.Lib.Utility;
+using Reloaded.Universal.Redirector.Lib.Utility.Native;
 using Reloaded.Universal.Redirector.Structures;
 
 namespace Reloaded.Universal.Redirector.Benchmarks.Benchmarks;
 
 [MemoryDiagnoser, DisassemblyDiagnoser(int.MaxValue, printInstructionAddresses: true, printSource: true)]
-public class RecursionLockCandidatesBenchmark : IBenchmark
+public unsafe class RecursionLockCandidatesBenchmark : IBenchmark
 {
     private bool _field;
     
@@ -14,7 +15,16 @@ public class RecursionLockCandidatesBenchmark : IBenchmark
     private static object _lock = new object();
     private int _fieldInterlocked;
     private SemaphoreRecursionLock _semaphore = new();
-    private NativeIntList _intList = new();
+    private delegate*unmanaged[Stdcall, SuppressGCTransition]<int> _getCurrentThreadId;
+
+    public RecursionLockCandidatesBenchmark()
+    {
+        var kernel32 = Native.LoadLibraryW("kernelbase.dll");
+        if (kernel32 == 0) // old OS
+            kernel32 = Native.LoadLibraryW("kernel32.dll");
+        
+        _getCurrentThreadId = (delegate*unmanaged[Stdcall, SuppressGCTransition]<int>)Native.GetProcAddress(kernel32, "GetCurrentThreadId");
+    }
 
     [Benchmark]
     public bool SemaphoreLock()
@@ -31,7 +41,7 @@ public class RecursionLockCandidatesBenchmark : IBenchmark
     [Benchmark]
     public unsafe bool SemaphoreLockNativeThreadId()
     {
-        var threadId = _intList.GetCurrentThreadId();
+        var threadId = _getCurrentThreadId();
         if (_semaphore.IsThisThread(threadId))
             return true;
 
